52bf8ad7969672fd3d871029f53563d0a240a4bb
Bosanxx <jupitermx858@gmail.com>
2021-10-13 23:23:35 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; Redmi 8A Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
+
+
+
+$ssid_ltc = "PHPSESSID=uvo873v3tc0kgetlbi28f4q486";
+
+$ssid_dgb = "PHPSESSID=7s60vup9ttc7mlfivt151lbvu0";
+
+$ssid_trx = "PHPSESSID=gqcu7p06eojm537j22uvqpc7t5";
+
+$ssid_doge = "PHPSESSID=8ar3e4p6btq3oc2jbtbndv9v07";
+
+$ssid_btc = "PHPSESSID=k70sunvhuo1vu8oogrpr23d7m2";
